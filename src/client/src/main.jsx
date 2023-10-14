9f21a9b56e1756bdc83db977a7a958092c0d0965
Harsh K <hersh.k17@gmail.com>
2023-10-13 21:30:37 -0400

    added basic react router implementation

--- src/client/src/main.jsx
+++ src/client/src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import ErrorPage from "./error-page";
+import "./index.css";
+import Contact, { action as contactAction, loader as contactLoader } from "./routes/contact";
+import { action as destroyAction } from "./routes/destroy";
+import EditContact, { action as editAction } from "./routes/edit";
+import Index from "./routes/index";
+import Root, { action as rootAction, loader as rootLoader } from "./routes/root";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />} loader={rootLoader} action={rootAction} errorElement={<ErrorPage />}>
+      <Route errorElement={<ErrorPage />}>
+        <Route index element={<Index />} />
+        <Route path="contacts/:contactId" element={<Contact />} loader={contactLoader} action={contactAction} />
+        <Route path="contacts/:contactId/edit" element={<EditContact />} loader={contactLoader} action={editAction} />
+        <Route path="contacts/:contactId/destroy" action={destroyAction} />
+      </Route>
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/client/src/routes/index.jsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <p id="zero-state">
+      This is a test of a React Router implementation. W.I.P
+    </p>
+  );
+}
